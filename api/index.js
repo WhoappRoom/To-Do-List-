f01cae468409ace85e03c7452a307635984398bb
Cabdi Waaxid Siciid <158821136+WhoappRoom@users.noreply.github.com>
2024-12-23 16:27:59 +0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const nova = require('novaxjs2');
+const path = require('path');
+const fs = require('fs');
+const app = nova();
+const port = 3000;
+
+const css = fs.readFileSync(path.join(__dirname, '../public/style.css'));
+const indexHtml = fs.readFileSync(path.join(__dirname, '../public/index.html'));
+const view = fs.readFileSync(path.join(__dirname, '../public/view.html'));
+const add = fs.readFileSync(path.join(__dirname, '../public/add.html'));
+
+// Set global styles
+app.style = css;
+
+// Handle GET request to show the to-do list page
+app.get('/', (req, res) => {
+  return indexHtml;
+});
+
+app.get('/view', (req, res) => {
+  return view;
+});
+app.get('/add', (req, res) => {
+  return add;
+});
+
+// Start the server
+app.at(port, () => {
+  console.log(`App is running on https://localhost:${port}`);
+});
+module.exports = app;
